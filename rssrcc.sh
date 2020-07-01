4e0b4d1a7973b6a42c2c48f2d4109f5270219447
samedamci <samedamci@disroot.org>
2020-07-01 16:23:40 +0200

    Optimalization

--- rssrcc.sh
+++ rssrcc.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-body() {
-  sed 's/#.*//g; /^$/d;
-  s/http/<outline xmlUrl=http/g;
-  s/ "/ \/> "/; s/xmlUrl=/xmlUrl="/g;
-  s/ \/>/" type="rss"\/>/g;
-  s/" *".*/"/g' "$1" | \
-    awk '{print "<outline title="$4, "text="$4">", $1, $2, $3, "</outline>"}'
-}
-
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <opml version=\"1.1\">
 <head>
 <title>Feeder</title>
 </head>
 <body>
-$(body "$1")
+$(sed 's/#.*//g; /^$/d' "$1" | awk '{print "<outline title="$2,
+"text="$2"><outline type=\"rss\" xmlUrl=\""$1 "\"/></outline>"}')
 </body>
 </opml>
 "
